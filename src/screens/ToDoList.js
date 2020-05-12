abac2e35964e1b659a8eebb62d6aa9d2d07e00a3
EduardoGiacomini <carloseduardo.diasgiacomini@gmail.com>
2020-05-11 22:10:32 -0400

    User can submit a new to-do to to-do list

--- src/screens/ToDoList.js
+++ src/screens/ToDoList.js
@@ -7,30 +7,73 @@ import { ListItem, ListItemLabel } from 'baseui/list';
 import { Check, Delete } from 'baseui/icon';
 
 class ToDoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: '',
+      todos: []
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.addToDo = this.addToDo.bind(this);
+    this.clearForm = this.clearForm.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.addToDo();
+    this.clearForm();
+  }
+
+  addToDo() {
+    this.setState({ todos: [...this.state.todos, this.state.value] });
+  }
+
+  clearForm() {
+    this.setState({ value: '' });
+  }
+
   render() {
+    const { value, todos } = this.state;
+
     return (
       <div>
         <DisplaySmall className="margin-bottom-3">Lista de tarefas</DisplaySmall>
-        <Paragraph1>Tarefa</Paragraph1>
-        <div className="flex-row margin-bottom-3">
-          <Input placeholder="Descrição da tarefa" />
-          <Button>Criar</Button>
-        </div>
-        <ListItem
-          endEnhancer={() => (
-            <React.Fragment>
-              <Button shape="round" size="compact" kind="secondary">
-                <Check />
-              </Button>
-              <div style={{ width: '18px' }} />
-              <Button shape="round" size="compact" kind="secondary">
-                <Delete />
-              </Button>
-            </React.Fragment>
-          )}
-        >
-          <ListItemLabel description="10 de maio - 21:56">Comprar pão</ListItemLabel>
-        </ListItem>
+        
+        <form onSubmit={this.handleSubmit}>
+          <Paragraph1>Tarefa</Paragraph1>
+          <div className="flex-row margin-bottom-3">
+            <Input value={value} onChange={this.handleChange} placeholder="Descrição da tarefa" />
+            <Button type="submit">Criar</Button>
+          </div>
+        </form>
+
+        {
+          todos.map((todo, index) => (
+            <ListItem
+              key={index}
+              endEnhancer={() => (
+                <React.Fragment>
+                  <Button shape="round" size="compact" kind="secondary">
+                    <Check />
+                  </Button>
+                  <div style={{ width: '18px' }} />
+                  <Button shape="round" size="compact" kind="secondary">
+                    <Delete />
+                  </Button>
+                </React.Fragment>
+              )}
+            >
+              <ListItemLabel description="10 de maio - 21:56">{todo}</ListItemLabel>
+            </ListItem>
+          ))
+        }
+
       </div>
     );
   }
